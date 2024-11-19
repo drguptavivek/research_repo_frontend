ace686d67e435d86f20c419d91212161153e35fe
VG <drguptavivek@yahoo.com>
2024-11-19 17:12:14 +0530

    dfdwq

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let name = 'Svelte';
+    let count = $state(0);
+    let numbers = $state([1, 2, 3, 4]);
+
+
+    let total = $derived(numbers.reduce((t, n) => t + n, 0));
+
+
+function increment() {
+    count += 1;
+}
+
+function addNumber() {
+    numbers.push(numbers.length + 1);
+}
+
+
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello {name.toUpperCase()}!</h1>
+
+
+<button class="bg-slate-400 m-2 p-2 border-purple-400 border-2" onclick={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{numbers.join(' + ')} = ...</p>
+
+<button onclick={addNumber}>
+	Add a number
+</button>
+
+<p>Total = {numbers.join(' + ')} = {total}</p>
+
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.500);
+    }
+  </style>
