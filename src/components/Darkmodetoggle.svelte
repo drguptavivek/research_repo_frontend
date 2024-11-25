18ea12a541487e4a3d9934deb3b2b88c872fa38e
VG <drguptavivek@yahoo.com>
2024-11-25 17:43:52 +0530

    Changed dar mode settins

---
+++ src/components/Darkmodetoggle.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { toggleMode } from "mode-watcher";
+    import { resetMode } from "mode-watcher";
+</script>
+
+<button on:click={toggleMode} class="btn">Dark/Light Mode</button>
+<button on:click={resetMode} class="btn">Reset Mode</button>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+
+<section class="container p-2 mx-auto">
+    <h1>About Us</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <button class="btn  btn-accent btn-outline">Button</button>
+</section>
